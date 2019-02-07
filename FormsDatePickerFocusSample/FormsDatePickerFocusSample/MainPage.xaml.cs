1071d404b6a68617e0cfa1855b6871723028682e
Will Beal <willbeal@outlook.com>
2019-02-07 14:34:18 +0100

    Add project files.

---
+++ FormsDatePickerFocusSample/FormsDatePickerFocusSample/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace FormsDatePickerFocusSample
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnMyButtonClicked(object sender, EventArgs e)
+        {
+            Device.BeginInvokeOnMainThread(() =>
+            {
+                if (MyDatePicker.IsFocused)
+                    MyDatePicker.Unfocus();
+
+                MyDatePicker.Focus();
+            });
+        }
+    }
+}
